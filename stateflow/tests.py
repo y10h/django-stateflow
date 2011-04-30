d025906e7ef911ff3beca4ad4bae0428d03bcde0
Yury Yurevich <yyurevich@jellycrystal.com>
2011-04-30 10:28:18 +0700

    Feature #1934: Update test to make it more readable and closer to reality.

--- stateflow/tests.py
+++ stateflow/tests.py
@@ -1,118 +1,125 @@
 from unittest import TestCase
 from django.db import models
 from django import forms
-from stateflow.stateclass import DjangoState, DjangoTransition, Flow, TransitionFailedError, IncorrectStateError
-from stateflow.statefields import StateFlowField, StateWidget
+from stateflow import stateclass, statefields
 
-class StatefulObject(object):
+class Article(object):
+
     state = None
 
     def __init__(self):
-        self.state = State1
+        self.state = New
 
 #States
-class State1(DjangoState):
+class New(stateclass.DjangoState):
     pass
 
-class State2(DjangoState):
+class Submitted(stateclass.DjangoState):
     pass
 
-class State3(DjangoState):
+class Approved(stateclass.DjangoState):
+    pass
+
+class Rejected(stateclass.DjangoState):
     pass
 
 #Transitions
-class StateTransition(DjangoTransition):
+class StateTransition(stateclass.DjangoTransition):
     abstract = True
-    
+
     @classmethod
     def apply(cls, obj, *args, **kwargs):
         if cls not in obj.state.forward_transitions:
-            raise IncorrectStateError("Cannot apply transition %s for state %s" %
-                             (cls, obj.state))
-        if cls not in obj.state.forward_allowed_transitions(kwargs.get('roles', [])):
-            raise TransitionFailedError("Cannot apply transition %s for state %s because of insufficient privileges" %
-                             (cls, obj.state))
+            raise stateclass.IncorrectStateError(
+                "Cannot apply transition %s for state %s" % (cls, obj.state))
+        if cls not in obj.state.forward_allowed_transitions([
+                kwargs.get('role')]):
+            raise stateclass.TransitionFailedError(
+                "Cannot apply transition %s for state %s because of "
+                "insufficient privileges" % (cls, obj.state))
         obj.state = cls.outcome
 
 
-class State1ToState2(StateTransition):
-    income = [State1]
-    outcome = State2
-    permissions = ['writer', 'owner']
+class Submit(StateTransition):
+    income = [New, Rejected]
+    outcome = Submitted
+    permissions = ['writer']
 
-class State12ToState3(StateTransition):
-    income = [State1, State2]
-    outcome = State3
-    permissions = ['owner']
+class Approve(StateTransition):
+    income = [Submitted]
+    outcome = Approved
+    permissions = ['editor']
 
-class State3ToState1(StateTransition):
-    income = [State3]
-    outcome = State1
-    permissions = ['owner']
+class Reject(StateTransition):
+    income = [Submitted]
+    outcome = Rejected
+    permissions = ['editor']
     forward = False
 
-#flow
-class StateFlow(Flow):
-    states = [State1, State2, State3]
-    transitions = [State1ToState2, State12ToState3]
-    initial_state = State1
+# flow
+class ArticleFlow(stateclass.Flow):
+    states = [New, Submitted, Approved, Rejected]
+    transitions = [Submit, Approve, Reject]
+    initial_state = New
 
-#abstract model with StateFlowField
-class StateModel(models.Model):
-    state = StateFlowField(flow=StateFlow)
+# abstract model with StateFlowField
+class ArticleModel(models.Model):
+    state = statefields.StateFlowField(flow=ArticleFlow)
 
     class Meta():
         abstract=True
 
-#form with StateWidget
-class StateModelForm(forms.ModelForm):
+# form with StateWidget
+class ArticleModelForm(forms.ModelForm):
     class Meta():
-        model = StateModel
+        model = ArticleModel
 
 class TransitionTest(TestCase):
 
     def test_forward(self):
-        obj = StatefulObject()
-        self.assertEquals(obj.state, State1)
+        obj = Article()
+        self.assertEquals(obj.state, New)
 
-        State1ToState2.apply(obj, roles=['owner'])
-        self.assertEquals(obj.state, State2)
+        Submit.apply(obj, role='writer')
+        self.assertEquals(obj.state, Submitted)
 
 
     def test_wrong_apply(self):
-        obj = StatefulObject()
-        self.assertEquals(obj.state, State1)
+        obj = Article()
+        self.assertEquals(obj.state, New)
 
-        State12ToState3.apply(obj, roles=['owner'])
-        self.assertEquals(obj.state, State3)
-
-        self.assertRaises(IncorrectStateError,
-                          State12ToState3.apply, obj, roles=['owner'])
+        self.assertRaises(stateclass.IncorrectStateError,
+                          Reject.apply, obj, role='editor')
 
     def test_permissions(self):
-        obj = StatefulObject()
-        self.assertEquals(obj.state, State1)
+        obj = Article()
+        self.assertEquals(obj.state, New)
 
-        State1ToState2.apply(obj, roles=['writer'])
-        self.assertEquals(obj.state, State2)
+        Submit.apply(obj, role='writer')
+        self.assertEquals(obj.state, Submitted)
 
-        self.assertRaises(TransitionFailedError,
-                          State12ToState3.apply, obj, roles=['writer'])
+        self.assertRaises(stateclass.TransitionFailedError,
+                          Approve.apply, obj, role='writer')
 
     def test_backward(self):
-        obj = StatefulObject()
-        self.assertEquals(obj.state, State1)
+        obj = Article()
+        self.assertEquals(obj.state, New)
 
-        State12ToState3.apply(obj, roles=['owner'])
-        self.assertEquals(obj.state, State3)
+        Submit.apply(obj, role='writer')
+        self.assertEquals(obj.state, Submitted)
 
-        State3ToState1.apply(obj, roles=['owner'])
-        self.assertEquals(obj.state, State1)
+        Reject.apply(obj, role='editor')
+        self.assertEquals(obj.state, Rejected)
+
+        Submit.apply(obj, role='writer')
+        self.assertEquals(obj.state, Submitted)
+
 
 class StateFieldTest(TestCase):
     def test_form(self):
-        form = StateModelForm(instance=StateModel(state=State2))
+        form = ArticleModelForm(instance=ArticleModel(state=Submitted))
 
-        self.assertTrue(isinstance(form.fields['state'].widget, StateWidget))
+        self.assertTrue(isinstance(form.fields['state'].widget,
+                                   statefields.StateWidget))
         self.assertEquals(len(form.fields['state'].choices),
-                          len(StateFlow.states) + 1)
+                          len(ArticleFlow.states) + 1)
